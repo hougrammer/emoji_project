6c09e0b77d9779064ef99fd4271178496c403d33
David Hou <david.m.hou@gmail.com>
2018-10-21 22:23:24 -0700

    simple embedding from keywords

---
+++ utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy
+from importlib import reload
+from IPython.core.display import display, HTML
+
+def cosine_similarity(v1, v2):
+    '''
+    args:
+        v1: numeric iterable
+        v2: numeric iterable
+    returns:
+        (float) cosine similarity of the two vectors
+    '''
+    return 1 - scipy.spatial.distance.cosine(v1, v2)
+
+def display_emoji(unicode, size=28):
+    '''
+    renders emoji in html
+    '''
+    display(HTML('<p style="font-size: {}px">{}</p>'.format(size, chr(int(unicode[2:], 16)))))
