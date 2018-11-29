c2fdb96ac2f47077cdebe4a6d4bd1b2e8286b9eb
David Hou <david.m.hou@gmail.com>
2018-11-29 09:32:34 -0800

    lstm moby dick

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy
 from importlib import reload
 from IPython.core.display import display, HTML
+import tensorflow as tf
 
 def cosine_similarity(v1, v2):
     '''
@@ -21,3 +22,18 @@ def display_emoji(unicode, size=2):
     '''
     display(HTML('<font size="+{}">{}</font>'.format(size, chr(int(unicode[2:], 16)))))
     
+def matmul3d(X, W):
+    """Wrapper for tf.matmul to handle a 3D input tensor X.
+    Will perform multiplication along the last dimension.
+
+    Args:
+      X: [m,n,k]
+      W: [k,l]
+
+    Returns:
+      XW: [m,n,l]
+    """
+    Xr = tf.reshape(X, [-1, tf.shape(X)[2]])
+    XWr = tf.matmul(Xr, W)
+    newshape = [tf.shape(X)[0], tf.shape(X)[1], tf.shape(W)[1]]
+    return tf.reshape(XWr, newshape)
